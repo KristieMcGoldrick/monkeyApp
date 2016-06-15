1ca754ee6e749c55e389c42d55e54f6124e3bf71
Laney Smith <lsmith.unc@gmail.com>
2016-06-15 16:59:57 -0600

    Working crud app

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,31 @@ router.get('/create', function(req, res, next) {
   res.render('create');
 });
 
+router.get('/:id', function(req, res, next) {
+  knex('cebus').where({id: req.params.id}).first().then(function(data) {
+    res.render('detail', {monkey: data});
+  })
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  knex('cebus').where({id: req.params.id}).first().then(function(data) {
+    res.render('edit', {monkey: data})
+  })
+});
+
+router.post('/:id/edit', function(req, res, next) {
+  knex('cebus').where({id: req.params.id}).update(req.body).then(function () {
+    res.redirect('/' + req.params.id);
+  })
+});
+
+// Delete a monkey
+router.get('/:id/delete', function(req, res, next) {
+  knex('cebus').where({id: req.params.id}).del().then(function(data) {
+    res.redirect('/');
+  })
+});
+
 router.post('/create', function(req, res, next) {
   knex('cebus').insert(req.body).then(function(){
     res.redirect('/');
@@ -21,6 +46,5 @@ router.post('/create', function(req, res, next) {
     console.log(error);
   })
 });
-
 
 module.exports = router;
