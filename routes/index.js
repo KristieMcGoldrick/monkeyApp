2953d93d375d67d98227bdeea2fa6588874833a8
Laney Smith <lsmith.unc@gmail.com>
2016-06-15 16:06:30 -0600

    Add create.hbs with insert monkey form & routes for adding new monkey's to local database & rendering on index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('cebus').select().then(function(data) {
+    res.render('index', {cebus: data})
+  })
 });
+
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
+router.post('/create', function(req, res, next) {
+  knex('cebus').insert(req.body).then(function(){
+    res.redirect('/');
+  }).catch(function(error) {
+    console.log(error);
+  })
+});
+
 
 module.exports = router;
